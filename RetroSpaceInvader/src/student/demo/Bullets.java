53fd7a4f019f36593c386c09a94c6fe394dbec3c
wtc9471 <33388701+wtc9471@users.noreply.github.com>
2017-11-09 21:49:54 +0800

    Add files via upload

---
+++ RetroSpaceInvader/src/student/demo/Bullets.java
@@ -0,0 +1,66 @@
+package student.demo;
+
+import game.v2.Console;
+import java.awt.Image;
+
+public class Bullets {
+
+    private Image img = Console.loadImage("/student/demo/img/bullet.png");
+    private int x=0;
+    private int y=-100;
+    private int hbx;
+    private int hby;
+    private int width = 2;
+    private int height = 5;
+
+    public void bulletInit(int a, int b) {
+        x = a+32;
+        y = b+50;
+    }
+
+    public void bulletMove(int a) {
+        y = y + a;
+        hbx = x;
+        hby = y;
+    }
+
+    public boolean onScreen() {
+        if (y < 600) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getx() {
+        return x;
+    }
+
+    public int gety() {
+        return y;
+    }
+
+    public Image getimg() {
+        return img;
+    }
+
+    public void destroyBullets() {
+        y = -1000;
+    }
+
+    public int getHbx() {
+        return hbx;
+    }
+
+    public int getHby() {
+        return hby;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
